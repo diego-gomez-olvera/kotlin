7048b48fad488b3ede54763006ae15fd53df80de
Vladimir Sukharev <vladimir.sukharev@jetbrains.com>
2022-06-07 18:04:52 +0200

    Add unittest for exception raised at runtime for KT35544

--- kotlin-native/backend.native/tests/interop/objc/tests/kt26478.kt
+++ kotlin-native/backend.native/tests/interop/objc/tests/kt26478.kt
@@ -11,11 +11,11 @@ fun testKT26478compiletime() {
     assertEquals("class NSBundle cannot be cast to class KT26478", exception.message)
 }
 
-val bundle:Any = NSBundle()
+val bundle26478:Any = NSBundle()
 @Test
 fun testKT26478runtime() {
     val exception = assertFailsWith<ClassCastException> {
-        bundle as KT26478
+        bundle26478 as KT26478
     }
     assertEquals("class NSBundle cannot be cast to class KT26478", exception.message)
 }
